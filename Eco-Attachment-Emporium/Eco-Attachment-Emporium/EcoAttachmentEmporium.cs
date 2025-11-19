363fa5d434480436f52b50591f4745ba7d3f57b6
Eco9341 <mastronardiluca7@gmail.com>
2025-11-19 20:51:36 +0100

    Added Source Code
    e2

---
+++ Eco-Attachment-Emporium/Eco-Attachment-Emporium/EcoAttachmentEmporium.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using WTTServerCommonLib.Models;
+using Range = SemanticVersioning.Range;
+
+namespace EcoAttachmentEmporium;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.wtt.EcoAttachmentEmporium";
+    public override string Name { get; init; } = "Eco-Attachment-Emporium";
+    public override string Author { get; init; } = "Eco";
+    public override List<string>? Contributors { get; init; } = null;
+    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public override Range SptVersion { get; init; } = new("~4.0.6");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string License { get; init; } = "MIT";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 20)]
+public class EcoAttachmentEmporium(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon) : IOnLoad
+{
+
+    public async Task OnLoad()
+    {
+        
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await Task.CompletedTask;
+    }
+}
