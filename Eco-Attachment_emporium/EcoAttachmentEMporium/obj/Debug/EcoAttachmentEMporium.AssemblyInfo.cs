fb8ab5034614fbd4c4f185b243a17a7daefd8ef5
Eco9341 <mastronardiluca7@gmail.com>
2026-04-10 14:19:04 +0200

    Updated stuff

--- Eco-Attachment_emporium/EcoAttachmentEMporium/obj/Debug/EcoAttachmentEMporium.AssemblyInfo.cs
+++ Eco-Attachment_emporium/EcoAttachmentEMporium/obj/Debug/EcoAttachmentEMporium.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eco-Attachment-Emporium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.6.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.6+7290d68d51619faf62bb0cbc8d247b75d632b29f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.6+2a6b8b9259edc5deaa180fa1dc717771e193e321")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eco-Attachment-Emporium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eco-Attachment-Emporium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.6.0")]
