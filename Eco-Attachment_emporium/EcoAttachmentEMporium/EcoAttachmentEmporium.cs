830a675507ae948624ad7c1023ba0e14a7f7d108
Eco9341 <mastronardiluca7@gmail.com>
2026-08-04 13:42:17 +0200

    Update code

--- Eco-Attachment_emporium/EcoAttachmentEMporium/EcoAttachmentEmporium.cs
+++ Eco-Attachment_emporium/EcoAttachmentEMporium/EcoAttachmentEmporium.cs
@@ -8,37 +8,36 @@ using EcoAttachmentEmporium.Helpers;
 
 namespace EcoAttachmentEmporium;
 
-public record ModMetadata : AbstractModMetadata
+public record ModMetadata : IModMetadata
 {
-    public override string ModGuid { get; init; } = "com.wtt.ecoattachmentemporium";
-    public override string Name { get; init; } = "Eco-Attachment-Emporium";
-    public override string Author { get; init; } = "Eco";
-    public override List<string>? Contributors { get; init; } = null;
-    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
-    public override Range SptVersion { get; init; } = new("~4.0.x");
-    public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    public string ModGuid { get; init; } = "com.wtt.ecoattachmentemporium";
+    public string Name { get; init; } = "Eco-Attachment-Emporium";
+    public string Author { get; init; } = "Eco";
+    public List<string>? Contributors { get; init; } = null;
+    public SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public Range SptVersion { get; init; } = new("~4.1.1");
+    public List<string>? Incompatibilities { get; init; }
+    public Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.20") }
+        { "com.wtt.commonlib", new Range("~3.0.3") }
     };
-    public override string? Url { get; init; }
-    public override bool? IsBundleMod { get; init; } = true;
-    public override string License { get; init; } = "MIT";
+    public string? Url { get; init; }
+    public bool HasPrepatcher { get; init; } = false;
+    public string License { get; init; } = "MIT";
 }
 
 
-[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
-public class EcoAttachmentEmporium(
+[Injectable(TypePriority = OnLoadOrder.Preload + 2)]
+public class DoomArcadeServer(
     WTTServerCommonLib.WTTServerCommonLib wttCommon,
-       EcoQuestHelper ecoQuestHelper) : IOnLoad
+    EcoQuestHelper ecoQuestHelper) : IOnLoad
 {
-    public async Task OnLoad()
+    public async Task OnLoadAsync(CancellationToken cancellationToken)
     {
         Assembly assembly = Assembly.GetExecutingAssembly();
         await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
         await wttCommon.CustomBotLoadoutService.CreateCustomBotLoadouts(assembly);
         await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
         ecoQuestHelper.ModifyQuests();
-        await Task.CompletedTask;
     }
 }
--- Eco-Attachment_emporium/EcoAttachmentEmporium/Helpers/EcoQuestHelper.cs
+++ Eco-Attachment_emporium/EcoAttachmentEmporium/Helpers/EcoQuestHelper.cs
@@ -1,14 +1,12 @@
-﻿using SPTarkov.DI.Annotations;
-using SPTarkov.Server.Core.Models.Common;
-using SPTarkov.Server.Core.Models.Eft.Common.Tables;
-using SPTarkov.Server.Core.Models.Utils;
-using SPTarkov.Server.Core.Services;
+﻿using SPTarkov.Common.Models.Logging;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.Spt.Tables;
 using WTTServerCommonLib.Helpers;
 
 namespace EcoAttachmentEmporium.Helpers
 {
     [Injectable]
-    public class EcoQuestHelper(DatabaseService  databaseService, ISptLogger<EcoQuestHelper> logger, QuestHelper questHelper)
+    public class EcoQuestHelper(TemplateTable templateTable, ISptLogger<EcoQuestHelper> logger, QuestHelper questHelper)
     {
 
         // Define weapon IDs
@@ -84,7 +82,7 @@ namespace EcoAttachmentEmporium.Helpers
         
         public void ModifyQuests()
         {
-            var quests = databaseService.GetTemplates().Quests;
+            var quests = templateTable.Quests;
 
             // ReSharper disable CommentTypo
             // ====================== PRAPOR QUESTS ======================
